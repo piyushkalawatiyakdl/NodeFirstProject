fd177ba251d0c1c4d36fd036d2c01733cf8543ce
unknown <piyushkalawatiyakdl@gmail.com>
2023-06-20 11:18:04 +0530

    three routing pages added

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const http= require("http")
 
 const server= http.createServer((req,res)=>{
+    if(req.url=="/home"){
 
-    // res.send("<h1>hello</h1>")
-    console.log("hello")
+        res.end("hello from piyush")
+    }
+    if(req.url=="/about"){
+
+        res.end("about page")
+    }if(req.url=="/contact"){
+
+        res.end("contact page")
+    }
+
 
 })
 
-server.listen(3000)
+server.listen(3000,"127.0.0.1",()=>{
+    console.log("Running on port 3000")
+})
+
